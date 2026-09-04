3c9b192fa5da59c42a167e65c87acb9527f535cf
nitin-kr081 <nitin.25bcs10652@sst.scaler.com>
2026-09-05 00:14:17 +0530

    Token generator for login user

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -1,5 +1,6 @@
 import User from '../models/user.models.js'
 import bcrypt from 'bcrypt'
+import {genToken} from "../utils/generateTokens.js"
 
 // Register Controller
 
@@ -7,13 +8,14 @@ const cookiesOption = {
     httpOnly : true,
     secure : true
 }
+
 export const registerUser = async(req , res) =>{
     try{
         const {fullName, email, password, phone} = req.body
 
         // All fields are mandatory
         if(!fullName || !email || !password || !phone){
-            res.status(400).json({
+            return res.status(400).json({
                 message : "All fields are mandatory"
             })
         }
@@ -21,21 +23,21 @@ export const registerUser = async(req , res) =>{
         // Email must be unique
         const emailExists = await User.findOne({email})
         if(emailExists){
-            res.status(409).json({
+            return res.status(409).json({
                 message : "Email Already in Use"
             })
         }
 
         // Password must contain at least 6 characters
         if(password.length < 6){
-            res.staus(400).json({
+            return res.staus(400).json({
                 message : "Please use a Strong Password"
             })
         }
 
         // Phone number is required
         if(!phone){
-            res.status(400).json({
+            return res.status(400).json({
                 message : "Please Enter Phone Number"
             })
         }
@@ -55,9 +57,56 @@ export const registerUser = async(req , res) =>{
         const token = genToken(newUser._id)
         res.cookie('token' , token , cookiesOption)
 
-        res.status(201).json({success : true, message: "Customer registered successfully", user: newUser })
+        res.status(201).json({success : true, message: "Customer registered successfully", 
+            user: {
+                id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                phone: newUser.phone
+            }
+         })
 
     }catch(error){
+        res.status(500).json({ message: 'Internal Server Errorr', error: error })
+    }
+}
+
+// Login Controller
+
+export const loginUser = async(req , res) =>{
+    try{
+        const {email , password} = req.body
+        const user = await User.findOne({email})
         
+        if(!user){
+            return res.status(401).json({
+                message : "Invalid Credentials"
+            })
+        }
+
+        const passwordCheck = await bcrypt.compare(password , user.password)
+
+        if(!passwordCheck){
+            return res.status(401).json({
+                message : "Invalid Credentials"
+            })
+        }
+
+        // Generate tokens
+        const token = genToken(user._id)
+        res.cookie('token' , token , cookiesOption)
+
+        return res.status(200).json({
+            "success": true,
+            "message": "Login successful",
+            user: {
+                id: user._id,
+                fullName: user.fullName,
+                email: user.email,
+                phone: user.phone
+            }
+        })
+    }catch(error){
+        res.status(500).json({ message: 'Internal Server Errorr', error: error })
     }
 }
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import {registerUser} from '../controllers/user.controllers.js'
+import {loginUser} from '../controllers/user.controllers.js'
 
 const userRoutes = express.Router()
 
 // Register
 userRoutes.post('/register' , registerUser)
+
+// Login
+userRoutes.post('/login' , loginUser)
 
 export default userRoutes
