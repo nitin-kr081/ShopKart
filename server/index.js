43695e9bb8fa0877a3fc4c6a5392749a9a6a2606
nitin-kr081 <nitin.25bcs10652@sst.scaler.com>
2026-09-04 14:41:57 +0530

    connected database

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
 const app = express()
 
 const PORT = 8180
 
+dotenv.config()
 
+mongoose.connect(process.env.dbURL).then(()=>{
+    console.log("DB Connected")
+}).catch((error)=>{
+    console.log(error)
+})
+
+app.use(express.json())
 
 app.listen(PORT , (req , res)=>{
     console.log(`Server Started at ${PORT}`)
